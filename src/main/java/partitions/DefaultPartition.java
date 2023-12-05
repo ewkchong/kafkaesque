74d5571d3c6211b48030317512a064fc31d1b703
Keigo <keigol@students.cs.ubc.ca>
2023-12-04 23:35:46 -0800

    typo

--- src/main/java/partitions/DefaultPartition.java
+++ src/main/java/partitions/DefaultPartition.java
@@ -12,7 +12,7 @@ public abstract class DefaultPartition implements Partition {
 
     public DefaultPartition(Topic topic) {
         this.topic = topic;
-        log = new InternalLog(new LogConfig(999, "note sure what to put"));
+        log = new InternalLog(new LogConfig(999, "not_sure_what_to_put"));
     }
 
     public Topic getTopic() {
